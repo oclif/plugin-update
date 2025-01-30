055fd2f6300cef26a1b4033f74e895a6221dee49
Mike Donnalley <mdonnalley@salesforce.com>
2025-01-30 09:19:50 -0700

    chore(W-17692101): eslint 9 (#1031)

--- examples/s3-update-example-cli/src/commands/hello.ts
+++ examples/s3-update-example-cli/src/commands/hello.ts
@@ -1,23 +1,20 @@
 import {Command, flags} from '@oclif/command'
 
 export default class Hello extends Command {
-  static description = 'describe the command here'
-
-  static examples = [
+  static args = [{name: 'file'}]
+static description = 'describe the command here'
+static examples = [
     `$ s3-update-example-cli hello
 hello world from ./src/hello.ts!
 `,
   ]
-
-  static flags = {
+static flags = {
+    // flag with no value (-f, --force)
+    force: flags.boolean({char: 'f'}),
     help: flags.help({char: 'h'}),
     // flag with a value (-n, --name=VALUE)
     name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
   }
-
-  static args = [{name: 'file'}]
 
   async run() {
     const {args, flags} = this.parse(Hello)
